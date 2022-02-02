e642f225579ad4f319264b516514f040b0d1a0a2
MDANDEL <maxime.dandel@suez.com>
2022-02-02 20:17:22 +0100

    delete and post method added

--- API/db_weather_station.sql
+++ API/db_weather_station.sql
@@ -8,3 +8,5 @@ CREATE TABLE Weather_Station_Test.Sonde1 (
     `Added` TIMESTAMP NOT NULL DEFAULT current_timestamp
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 INSERT INTO Weather_Station_Test.sonde1 (Temperature, Humidity) values (30,70), (30.5, 69.9), (40.5, 15.9), (15.5, 78.6);
+DELETE FROM weather_station_test.`sonde1` WHERE `id`=56;
+
--- API/main.py
+++ API/main.py
@@ -4,6 +4,7 @@ from secrets import token_urlsafe
 import mysql.connector
 import datetime
 
+
 app = Flask(__name__)
 
 api = Api(app=app, version="0.1", doc="/api", title="Mon API", description="ceci est une description de l'api de test",
@@ -52,7 +53,7 @@ def get_sql_from_file(filename):
 
 try:
     with db.cursor() as cursor:
-        sql = "SELECT `id` FROM `sonde1` WHERE `id`=%s;"
+        sql = "SELECT `id` FROM weather_station_test.`sonde1` WHERE `id`=%s;"
         cursor.execute(sql)
         result = cursor.fetchone()
         print("Il existe au moins un article dans la DB")
@@ -92,54 +93,91 @@ class temperature(Resource):
         """
         Retourner un utilisateur pour un ID donné
         """
-        sql = "SELECT `id`, CAST(`temperature` as double) as temperature, CAST(`humidity` as double) as humidity, CAST(`added` as CHAR) as added FROM `sonde1` WHERE `id`=%s"
+        sql = "SELECT `id`, CAST(`temperature` as double) as temperature, CAST(`humidity` as double) as humidity, CAST(`added` as CHAR) as added FROM weather_station_test.`sonde1` WHERE `id`=%s"
         with db.cursor(dictionary=True) as cursor:
             cursor.execute(sql, (id,))
             result = cursor.fetchone()
         return result
 
 
-@app.route('/')
-def index():
-    return 'index'
+@api.route("/api/v1/delete/temperature/<int:id>", methods=['DELETE'])
+class temperature(Resource):
+    def delete(self, id):
+        """Supprimer une ligne de données"""
+        sql = "DELETE FROM weather_station_test.`sonde1` WHERE `id`="+str(id)
+        with db.cursor(dictionary=True) as cursor:
+            print(sql)
+            result = cursor.execute(sql)
+            db.commit()
+            print(result)
+            # if int(result) > 0:
+            #     return {'user_deleted_with_success': str(result)}, 200
+            # else:
+            #     return {'error': 'no user found to delete'}, 204
+    # def get(self, id):
+    #     """
+    #     Retourner un utilisateur pour un ID donné
+    #     """
+    #     sql = "SELECT `id`, CAST(`temperature` as double) as temperature, CAST(`humidity` as double) as humidity, CAST(`added` as CHAR) as added FROM `sonde1` WHERE `id`=%s"
+    #     with db.cursor(dictionary=True) as cursor:
+    #         cursor.execute(sql, (id,))
+    #         result = cursor.fetchone()
+    #     return result
+@api.route("/api/v1/add", methods=['POST'])
+class temperature(Resource):
+    def post(self):
+        """Supprimer une ligne de données"""
+        #sql = "INSERT INTO weather_station_test.`sonde1` VALUES (`temperature`, `humidity`)"
+        sql = "INSERT INTO weather_station_test.`sonde1` (`temperature`, `humidity`) VALUES (%s, %s)"
+        with db.cursor(dictionary=True) as cursor:
+            result = cursor.execute(sql, (20.6, 51.2))
+            db.commit()
+            last_id = cursor.lastrowid
+        return {'user created with id': str(last_id)}, 200
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return do_the_login()
-    else:
-        return show_the_login_form()
-
-
-@app.route('/user/<username>')
-@login_required
-def profile(username):
-    return f'{username}\'s profile'
-
-
-@app.route('/logout')
-def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('index'))
-
-
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
-
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-
-@app.route('/about')
-def about():
-    return 'The about page'
+#
+# @app.route('/')
+# def index():
+#     return 'index'
+#
+#
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
+#
+#
+# @app.route('/user/<username>')
+# @login_required
+# def profile(username):
+#     return f'{username}\'s profile'
+#
+#
+# @app.route('/logout')
+# def logout():
+#     # remove the username from the session if it's there
+#     session.pop('username', None)
+#     return redirect(url_for('index'))
+#
+#
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('login'))
+#     print(url_for('login', next='/'))
+#     print(url_for('profile', username='John Doe'))
+#
+#
+# @app.route('/projects/')
+# def projects():
+#     return 'The project page'
+#
+#
+# @app.route('/about')
+# def about():
+#     return 'The about page'
 
 
 # @app.route("/")
