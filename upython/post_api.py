5a947363ded633b2a2029dadf8e81b0fd132aea0
MDANDEL <92440390+MDANDEL@users.noreply.github.com>
2022-02-03 22:33:39 +0100

    test file should work, if yes copy paste to the boot

--- upython/post_api.py
+++ upython/post_api.py
@@ -52,87 +52,30 @@ def run_sensor1():
 
 
 while True:
-    def get_measures(mode="all", measures=10, interval=1):
-        from machine import I2C, Pin
-        import time
-        i2c = I2C(scl=Pin(0), sda=Pin(2))
-        s = SI7021(i2c)
-        temp_data = []
-        humi_data = []
-        fahr_data = []
-        #print("On va prendre "+str(measures)+" mesures a "+str(interval)+" secondes d'intervalle")
-        while measures > 0:
-            #print("mesure restante "+str(measures)+"\nTemperature:"+str(s.temperature())+"\nHumidite:"+str(s.humidity()))
-            temp_data.append(s.temperature())
-            humi_data.append(s.humidity())
-            fahr_data.append(s.fahrenheit())
-            measures -= 1
-            time.sleep(interval)
-        print({"all temp":temp_data})
-        print({"all hum":humi_data})
-        temperature = round(sum(temp_data)/len(temp_data),1)
-        humidite = round(sum(humi_data)/len(humi_data),1)
-        fahrenheit = round(sum(fahr_data)/len(fahr_data),1)
-        if mode == "all":
-            data = {"temperature": temperature, "humidite": humidite}
-        elif mode == "temperature":
-            data = {"temperature": temperature}
-        elif mode == "humidite":
-            data = {"humidite": humidite}
-        else:
-            data = {"average temp": temperature, "average hum": humidite}
-        return data
-    get_measures(mode="all", measures=5, interval=1)
-    
-    
-    i2c = machine.I2C(machine.Pin(0), machine.Pin(2))
-    temp_sensor = si7021.Si7021(i2c)
-    sensor = value=temp_sensor.identifier
-    temperature = temp_sensor.temperature
-    humidity = temp_sensor.relative_humidity
-    fahrenheit = si7021.convert_celcius_to_fahrenheit(temp_sensor.temperature)
-    
-    temp_data = []
-    humi_data = []
-    #print("On va prendre "+str(measures)+" mesures a "+str(interval)+" secondes d'intervalle")
-    while measures > 0:
-        #print("mesure restante "+str(measures)+"\nTemperature:"+str(s.temperature())+"\nHumidite:"+str(s.humidity()))
-        temp_data.append(s.temperature())
-        humi_data.append(s.humidity())
-        measures -= 1
-        time.sleep(interval)
-    print({"all temp":temp_data})
-    print({"all hum":humi_data})
-    temperature = round(sum(temp_data)/len(temp_data),1)
-    humidite = round(sum(humi_data)/len(humi_data),1)
-    if mode == "all":
-        data = {"temperature": temperature, "humidite": humidite}
-    elif mode == "temperature":
-        data = {"temperature": temperature}
-    elif mode == "humidite":
-        data = {"humidite": humidite}
-    else:
-        data = {"average temp": temperature, "average hum": humidite}
-    return data
-    get_measures(mode="all", measures=5, interval=1)
-    
-    
-    data = {"temperature":temperature, "humidite":humidity, "farenheit":fahrenheit, "capture":sensor}
-    print(data)
-    json_data = json.dumps(data).encode('utf-8')
-    headers = {'Content-Type':'application/json'}
-    print(data)
-    print(station.isconnected())
-    r = requests.post("http://192.168.43.60:5000/api/v1/ajouter/", data=json_data, headers=headers)
-    #r = requests.post("http://127.0.0.1:5000/api/v1/donnees/", json = data)
-    json_body= json.loads(r.text)
-    #json_body= r
-    print(json_body)
-    print('Temperature: %3.1f' %temperature)
-    print('Humidity: %3.1f' %humidity)
-    del temperature, humidity, data, json_data
-    sleep(10)
-  except OSError as e:
-    print('Failed')
-    print(repr(e))
+    try:
+        i2c = machine.I2C(machine.Pin(0), machine.Pin(2))
+        temp_sensor = si7021.Si7021(i2c)
+        sensor = temp_sensor.identifier
+        temperature = temp_sensor.temperature
+        humidity = temp_sensor.relative_humidity
+        fahrenheit = si7021.convert_celcius_to_fahrenheit(temp_sensor.temperature)
+
+        data = {"temperature":temperature, "humidite":humidity, "farenheit":fahrenheit, "capture":sensor}
+        print(data)
+        json_data = json.dumps(data).encode('utf-8')
+        headers = {'Content-Type':'application/json'}
+        print(data)
+        print(station.isconnected())
+        r = requests.post("http://192.168.43.60:5000/api/v1/ajouter/", data=json_data, headers=headers)
+        #r = requests.post("http://127.0.0.1:5000/api/v1/donnees/", json = data)
+        json_body= json.loads(r.text)
+        #json_body= r
+        print(json_body)
+        print('Temperature: %3.1f' %temperature)
+        print('Humidity: %3.1f' %humidity)
+        del temperature, humidity, data, json_data
+        sleep(10)
+     except OSError as e:
+        print('Failed')
+        print(repr(e))
     
--- upython/test.py
+++ upython/test.py
@@ -1,4 +1,3 @@
-from machine import Pin
 from time import sleep
 import urequests as requests
 import json
@@ -7,14 +6,12 @@ from machine import I2C, Pin
 import time
 import network
 import esp
+import si7021
 
 try:
   import usocket as socket
 except:
   import socket
-
-
-
 
 esp.osdebug(None)
 
@@ -48,32 +45,29 @@ def run_sensor1():
     print('Fahrenheit:          {value}'.format(value=si7021.convert_celcius_to_fahrenheit(temp_sensor.temperature)))
 
 
+ def get_measures(mode="all", measures=10, interval=1):
 
-while True:
-    def get_measures(mode="all", measures=10, interval=1):
-        from machine import I2C, Pin
-        import time
-        import si7021
         i2c = I2C(scl=Pin(0), sda=Pin(2))
-        s = si7021(i2c)
+        s = si7021.Si7021(i2c)
         temp_data = []
         humi_data = []
         fahr_data = []
         #print("On va prendre "+str(measures)+" mesures a "+str(interval)+" secondes d'intervalle")
         while measures > 0:
             #print("mesure restante "+str(measures)+"\nTemperature:"+str(s.temperature())+"\nHumidite:"+str(s.humidity()))
-            temp_data.append(s.temperature())
-            humi_data.append(s.humidity())
-            fahr_data.append(s.fahrenheit())
+            temp_data.append(s.temperature)
+            humi_data.append(s.relative_humidity)
+            fahr_data.append(si7021.convert_celcius_to_fahrenheit(s.temperature))
             measures -= 1
             time.sleep(interval)
         print({"all temp":temp_data})
         print({"all hum":humi_data})
+        print({"all fahr": fahr_data})
         temperature = round(sum(temp_data)/len(temp_data),1)
         humidite = round(sum(humi_data)/len(humi_data),1)
         fahrenheit = round(sum(fahr_data)/len(fahr_data),1)
         if mode == "all":
-            data = {"temperature": temperature, "humidite": humidite, "farenheit":fahrenheit, "capture":sensor}
+            data = {"temperature": temperature, "humidite": humidite, "farenheit":fahrenheit, "capture":s.identifier}
         elif mode == "temperature":
             data = {"temperature": temperature}
         elif mode == "humidite":
@@ -81,24 +75,28 @@ while True:
         elif mode == "farenheit":
             data = {"farenheit": fahrenheit}
         else:
-            data = {"temperature": temperature, "humidite": humidite, "farenheit": fahrenheit, "capture":sensor}
+            data = {"temperature": temperature, "humidite": humidite, "farenheit": fahrenheit, "capture":s.identifier}
         return data
-       
+
+
+while True:
+    data = get_measures(mode="all", measures=10, interval=1)
+    i2c = I2C(scl=Pin(0), sda=Pin(2))
+    s = si7021.Si7021(i2c)
     #data = {"temperature":temperature, "humidite":humidity, "farenheit":fahrenheit, "capture":sensor}
     print(data)
     json_data = json.dumps(data).encode('utf-8')
     headers = {'Content-Type':'application/json'}
     print(data)
     print(station.isconnected())
-    #r = requests.post("http://192.168.43.60:5000/api/v1/ajouter/", data=json_data, headers=headers)
+    r = requests.post("http://192.168.43.60:5000/api/v1/ajouter/", data=json_data, headers=headers)
     json_body= json.loads(r.text)
-    #json_body= r
     print(json_body)
-    print('Temperature: %3.1f' %temperature)
-    print('Humidity: %3.1f' %humidity)
-    del temperature, humidity, data, json_data
+    print('Temperature: %3.1f' %s.temperature)
+    print('Humidity: %3.1f' %s.humidity)
+    del  data, json_data
     sleep(10)
-    #except OSError as e:
-     #   print('Failed')
-      #  print(repr(e))
-    
+    except OSError as e:
+       print('Failed')
+       print(repr(e))
+
